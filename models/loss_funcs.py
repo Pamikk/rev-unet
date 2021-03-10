acc3587f2ba0f7a22de26ebc40424f04d017a023
snilloO <vega0.0@outlook.com>
2021-03-09 21:32:41 -0500

    finish network

--- models/loss_funcs.py
+++ models/loss_funcs.py
@@ -5,7 +5,7 @@ import numpy as np
 #Functional Utils
 mse_loss = nn.MSELoss()
 bce_loss = nn.BCELoss()
-def dice_loss1d(pd,gt,threshold=0.5):
+def dice_loss1d(pd,gt):
     assert pd.shape == gt.shape
     if gt.shape[0]==0:
         return 0
@@ -14,21 +14,17 @@ def dice_loss1d(pd,gt,threshold=0.5):
     gt_area = torch.sum(torch.pow(gt,2))
     dice = (2*inter+1)/(pd_area+gt_area+1)
     #fix nans
-    dice[dice != dice] = dice.new_tensor([1.0])
+    dice[dice != dice] = dice.new_tensor([1.0],device = pd.device)
     return 1-dice.mean()
 class LossAPI(nn.Module):
-    def __init__(self,cfg,loss):
+    def __init__(self,cfg):
         super(LossAPI,self).__init__()
-        self.not_match = 0
-
-    def forward(self,outs,gt=None,size=None,infer=False):
-        raise NotImplementedError
-        if infer:
-            return
+        self.loss = Losses[cfg.loss]
+    def forward(self,out,gt=None):
+        if (gt is None):
+            return torch.sigmoid(out)
         else:
-            return 
-    def reset_notmatch(self):
-        self.not_match = 0
+            return self.loss(out,gt)
 Losses = {}
 
 
--- models/network.py
+++ models/network.py
@@ -4,41 +4,46 @@ import torch.nn.functional as F
 import os
 import numpy as np
 
-from .backbone import ResNet,conv1x1,conv3x3,Darknet
+from .backbone import *
 from .loss_funcs import  LossAPI
-from .models import Darknetv3
-from .utils import init_weights
 networks = {}
-def NetAPI(cfg,net,loss,init=True):
+def NetAPI(cfg,init=True):
     raise NotImplementedError
-    if init:
-        network.initialization()
-    return network
-
-class NonResidual(nn.Module):
-    multiple=2
-    def __init__(self,in_channels,channels,stride=1):
-        super(NonResidual,self).__init__()
-        self.conv1 = conv1x1(in_channels,channels,stride)
-        self.relu = nn.LeakyReLU(0.1)
-        self.bn1 = nn.BatchNorm2d(channels,momentum=0.9, eps=1e-5)
-        self.conv2 = conv3x3(channels,channels*NonResidual.multiple)
-        self.bn2 = nn.BatchNorm2d(channels*NonResidual.multiple,momentum=0.9, eps=1e-5)
-    def forward(self,x):
-        y = self.conv1(x)
-        y = self.bn1(y)
-        y = self.relu(y)
-
-        y = self.conv2(y)
-        y = self.bn2(y)
-        y = self.relu(y)
-
-        return y
-class No_new_Net(nn.Module):
+class NoNewNet(nn.Module):
     def __init__(self,cfg):
-        super(No_new_Net,self).__init__()
-        self.channels = cfg.channels
+        super(NoNewNet,self).__init__()
+        channels = cfg.channels
+        out_channels = []
         self.out_channel = cfg.cls_num
+        encoder,decoder = Coders[cfg.net]
+        self.loss = LossAPI(cfg)
+        self.in_channel = cfg.channels
+        self.encoders = nn.ModuleList([])
+        self.decoders = nn.ModuleList([])
+        self.in_conv = BasicBlock(cfg.channel,self.in_channel)
+        self.out_conv = BasicBlock(cfg.cls_num,self.in_channel)
+        for channel in channels:
+            out_channels.insert(0,self.in_channel)
+            self.encoders.append(encoder(self.in_channel,channel,depth=cfg.depth))
+            self.in_channel = channel
+            
+        for channel in out_channels:
+            self.decoders.append(decoder(self.in_channel,channel))
+            self.in_channel = channel
+    def forward(self,x,gts=None):
+        feats = []
+        for i,encoder in enumerate(self.encoders):
+            if i!=0:
+                feats.insert(0,x)
+            x = encoder(x)
+            
+        for i,decoder in enumerate(self.decoders):
+            x = decoder(x)
+            if i<(len(feats)):
+                x+=feats[i]
+        return self.loss(x,gts)
+
+
 
     
 
